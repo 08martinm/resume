8c9f2008ac39f87f00c73a1d640df5f857f3cefb
Matt Martin <08martinm@gmail.com>
2017-08-23 12:17:48 -0700

    updated webpack to handle ico

--- config/webpack/loadImages.js
+++ config/webpack/loadImages.js
@@ -1,7 +1,7 @@
 const env = require('../env.js');
 
 exports.loadImages = {
-  test: /\.(png|svg|jpg|gif|woff)$/,
+  test: /\.(png|svg|jpg|gif|ico|woff)$/,
   use: [{
     loader: env ? 'url-loader' : 'file-loader',
     options: {limit: 25000, name: './[hash].[ext]'},
