92c64e728b7e1f55a794dd389cbb58126eebe599
Matt Martin <08martinm@gmail.com>
2017-08-23 12:26:48 -0700

    updated webpack config

--- config/webpack/devServer.js
+++ config/webpack/devServer.js
@@ -1,12 +1,12 @@
 const path = require('path');
 const env = require('../env.js');
 
-exports.devServer =
+exports.devServer = env === 'production' ? {} : 
   { 
     contentBase: path.join(__dirname, '../../public'),
     historyApiFallback: true,
     stats: 'normal',
-    hot: env,
+    hot: true,
     overlay: {
       errors: true,
       warnings: true,
