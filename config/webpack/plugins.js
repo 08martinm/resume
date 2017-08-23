c1ca7aa8c2d0270ee154ef2beacad13df2afcc1c
Matt Martin <08martinm@gmail.com>
2017-08-23 11:12:32 -0700

    added favicon

--- config/webpack/plugins.js
+++ config/webpack/plugins.js
@@ -11,6 +11,7 @@ const ExtractText = new ExtractTextPlugin({filename: '[name].[contenthash].css'}
 const HTMLWebpack = new HTMLWebpackPlugin({
   filename: path.join(__dirname, '../../public/index.html'),
   template: './src/index.html',
+  favicon: path.join(__dirname, '../../public/favicon.png'),
 });
 
 const LoaderOptions = new webpack.LoaderOptionsPlugin({
