0e333d5f5c3c20e5478119c225255795a179ba2e
Matt Martin <08martinm@gmail.com>
2017-08-23 13:07:41 -0700

    cleaning up server file

--- server/index.js
+++ server/index.js
@@ -2,8 +2,6 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const morgan = require('morgan');
 const port = process.env.PORT || 8080;
-// var Sequelize = require('sequelize');
-// var db = new Sequelize('resume', 'user', '', {host: 'postgres', dialect: 'postgres'});
 
 let app = express();
 
@@ -11,8 +9,4 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../public'));
 
-
-// db.sync({force: process.env.DROPTABLE})
-//   .then(app.listen(port, '0.0.0.0', () => {console.log('Server is listening on port', port);}));
-
-app.listen(port, '0.0.0.0', () => {console.log('Server is listening on port', port);});
+app.listen(port, () => {console.log('Server is listening on port', port);});
