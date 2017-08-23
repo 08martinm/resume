95b084efd91df9e1673d4df0d8889e151468cb26
Matt Martin <08martinm@gmail.com>
2017-08-23 11:27:40 -0700

    updated server

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const morgan = require('morgan');
 const port = process.env.PORT || 8080;
-var Sequelize = require('sequelize');
+// var Sequelize = require('sequelize');
 // var db = new Sequelize('resume', 'user', '', {host: 'postgres', dialect: 'postgres'});
 
 let app = express();
@@ -10,9 +10,9 @@ let app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../public'));
-
+app.get('/', function (req, res) { res.send('Hello World!'); });
 
 // db.sync({force: process.env.DROPTABLE})
 //   .then(app.listen(port, '0.0.0.0', () => {console.log('Server is listening on port', port);}));
 
-app.listen(port, '0.0.0.0', () => {console.log('Server is listening on port', port);})
+app.listen(port, () => {console.log('Server is listening on port', port);});
