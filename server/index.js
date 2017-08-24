30bafaed1bc0ac21a472548fe9aacb114e985c08
Matt <08martinm@gmail.com>
2017-08-23 19:49:25 -0700

    updated webpack config and server

--- server/index.js
+++ server/index.js
@@ -1,12 +1,7 @@
-const bodyParser = require('body-parser');
 const express = require('express');
-const morgan = require('morgan');
 const port = process.env.PORT || 8080;
 
 let app = express();
-
-app.use(morgan('dev'));
-app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../public'));
 
 app.listen(port, () => {console.log('Server is listening on port', port);});
